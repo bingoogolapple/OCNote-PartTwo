2e15eb59404b2e07ee5291f63c017b8bbd89e50e
bingoogolapple <bingoogolapple@gmail.com>
2015-04-07 01:56:43 +0800

    add Lottery

---
+++ Lottery/Lottery/Classes/Other/BGAAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Lottery
+//
+//  Created by bingoogol on 15/4/6.
+//  Copyright (c) 2015年 bingoogolapple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ Lottery/Lottery/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lottery
+//
+//  Created by bingoogol on 15/4/6.
+//  Copyright (c) 2015年 bingoogolapple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BGAAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGAAppDelegate class]));
+    }
+}
