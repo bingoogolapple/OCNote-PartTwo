3fd361af872f297f0ec7323ee518bd96ab46686e
bingoogolapple <bingoogolapple@gmail.com>
2015-05-19 21:43:44 +0800

    完成下拉刷新

--- WeiBo/WeiBo/Classes/Home/Controller/BGAHomeViewController.m
+++ WeiBo/WeiBo/Classes/Home/Controller/BGAHomeViewController.m
@@ -20,7 +20,7 @@
 
 @interface BGAHomeViewController ()<BGADropdownMenuDelegate>
 
-@property (nonatomic, strong) NSArray *statuses;
+@property (nonatomic, strong) NSMutableArray *statuses;
 
 @end
 
@@ -32,7 +32,95 @@
     
     [self setupUserInfo];
     
-    [self loadNewStatus];
+//    [self loadNewStatus];
+    
+    [self setupRefresh];
+}
+
+- (void)setupRefresh {
+    UIRefreshControl *control = [[UIRefreshControl alloc] init];
+    // 只有用户手动下拉刷新才会触发UIControlEventValueChanged事件
+    [control addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
+    [self.tableView addSubview:control];
+    
+    // 马上进入刷新状态(仅仅是像是刷新状态，并不会触发UIControlEventValueChanged事件)
+    [control beginRefreshing];
+    
+    [self refresh:control];
+}
+
+- (void)refresh:(UIRefreshControl *)control {
+    Logger(@"下拉刷新");
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    BGAAccount *account = [BGAAccountTool account];
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"access_token"] = account.access_token;
+    
+    BGAStatus *firstStatus = [self.statuses firstObject];
+    if (firstStatus) {
+        params[@"since_id"] = firstStatus.idstr;
+    }
+    
+    [manager GET:@"https://api.weibo.com/2/statuses/friends_timeline.json" parameters:params success:^(AFHTTPRequestOperation *operation, NSDictionary *responseObject) {
+        [control endRefreshing];
+        Logger(@"加载最新状态成功 - %@", responseObject);
+        NSArray *newStatuses = [BGAStatus objectArrayWithKeyValuesArray:responseObject[@"statuses"]];
+        
+        NSRange range = NSMakeRange(0, newStatuses.count);
+        NSIndexSet *set = [NSIndexSet indexSetWithIndexesInRange:range];
+        
+        [self.statuses insertObjects:newStatuses atIndexes:set];
+        
+        [self.tableView reloadData];
+        
+        [self showNewStatusCount:newStatuses.count];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [control endRefreshing];
+        Logger(@"加载最新状态失败 - %@", error);
+    }];
+}
+
+- (void)showNewStatusCount:(int)count {
+    UILabel *label = [[UILabel alloc] init];
+    label.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"timeline_new_status_background"]];
+    label.width = [UIScreen mainScreen].bounds.size.width;
+    label.height = 30;
+     
+    if (count == 0) {
+        label.text = @"没有新的微博数据，稍后再试";
+    } else {
+        label.text = [NSString stringWithFormat:@"共有%d条新的微博数据", count];
+    }
+    label.textColor = [UIColor whiteColor];
+    label.font = [UIFont systemFontOfSize:16];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.y = 64 - label.height;
+    
+    // 将label添加到导航控制器的view中，并且是盖在导航栏下
+    [self.navigationController.view insertSubview:label belowSubview:self.navigationController.navigationBar];
+    
+    CGFloat duration = 1.0;
+    // 如果某个动画执行完毕后，又要回到动画执行前的状态，建议使用transform来做动画
+    [UIView animateWithDuration:duration animations:^{
+//        label.y += label.height;
+        label.transform = CGAffineTransformMakeTranslation(0, label.height);
+    } completion:^(BOOL finished) {
+        CGFloat delay = 1.0;
+        [UIView animateWithDuration:duration delay:delay options:UIViewAnimationOptionCurveLinear animations:^{
+//            label.y -= label.height;
+            label.transform = CGAffineTransformIdentity;
+        } completion:^(BOOL finished) {
+            [label removeFromSuperview];
+        }];
+    }];
+}
+
+- (NSMutableArray *)statuses {
+    if (!_statuses) {
+        _statuses = [NSMutableArray array];
+    }
+    return _statuses;
 }
 
 - (void)loadNewStatus {
@@ -45,7 +133,9 @@
     
     [manager GET:@"https://api.weibo.com/2/statuses/friends_timeline.json" parameters:params success:^(AFHTTPRequestOperation *operation, NSDictionary *responseObject) {
         Logger(@"加载最新状态成功 - %@", responseObject);
-        self.statuses = [BGAStatus objectArrayWithKeyValuesArray:responseObject[@"statuses"]];
+        NSArray *newStatuses = [BGAStatus objectArrayWithKeyValuesArray:responseObject[@"statuses"]];
+        [self.statuses addObjectsFromArray:newStatuses];
+        
         [self.tableView reloadData];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         Logger(@"加载最新状态失败 - %@", error);
