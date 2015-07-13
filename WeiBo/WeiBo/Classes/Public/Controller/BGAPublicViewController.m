8e146a5f4df562449b6a2b6563b31a7fc950e620
bingoogolapple <bingoogolapple@gmail.com>
2015-07-14 00:21:36 +0800

    before 添加自定义UITextView

--- WeiBo/WeiBo/Classes/Public/Controller/BGAPublicViewController.m
+++ WeiBo/WeiBo/Classes/Public/Controller/BGAPublicViewController.m
@@ -24,14 +24,44 @@
     
     [self setupTextView];
     
+    
+    
 }
 
 - (void)setupTextView {
-    // UITextField只有一行
-    // UITextView可以有多行
+    
+    // UITextField只有一行，当有导航栏时内容会被导航栏遮住
+    // UITextView可以有多行，继承自ScrollView，会自定判断是否有导航栏来设置contentInset使内容不被导航来遮住
+    // 默认是YES:当scrollView遇到UINavigationBar、UITabBar等控件时，默认会设置ScrollView的contentInset
+    // UIViewController.automaticallyAdjustsScrollViewInsets = NO;
+    
+    /**
+     UITextField:
+     1.文字永远是一行，不能显示多行文字
+     2.有placehoder属性设置占位文字
+     3.继承自UIControl
+     4.监听行为
+     1> 设置代理
+     2> addTarget:action:forControlEvents:
+     3> 通知:UITextFieldTextDidChangeNotification
+     
+     UITextView:
+     1.能显示任意行文字
+     2.不能设置占位文字
+     3.继承自UIScollView
+     4.监听行为
+     1> 设置代理
+     2> 通知:UITextViewTextDidChangeNotification
+     */
+    
     UITextView *textView = [[UITextView alloc] init];
     textView.frame = self.view.bounds;
+    // 不会判断是否被导航栏遮住
+    // textView.y = 80;
+    // 按住option键，点击属性快速查看文档
     textView.font = [UIFont systemFontOfSize:18];
+    textView.backgroundColor = [UIColor redColor];
+    textView.textColor = [UIColor greenColor];
     [self.view addSubview:textView];
 }
 
